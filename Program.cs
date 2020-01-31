73e39f1859a543168ebdff7d1cee4d325f2d8a03
Daniel Widell <widell@usf.edu>
2020-01-31 01:38:16 -0500

    Added the code for the UserInput project, along with some helpful advice.

--- Program.cs
+++ Program.cs
@@ -6,9 +6,13 @@ namespace UserInputDanielWidell
     {
         static void Main(string[] args)
         {
-            string InstructionResponse;
-
+            string instructionResponse;
             Console.WriteLine("Hi, this is Daniel Widell.");
+            Console.WriteLine("Please type lymph and press enter.");
+            instructionResponse = Console.ReadLine();
+            Console.WriteLine("You typed '{0}'", instructionResponse);
+            Console.WriteLine("Maybe you should get your nodes checked out.");
+           
         }
     }
 }
