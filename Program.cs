c5dac576932609c68b1719470c6d5d7874774e03
Daniel Widell <widell@usf.edu>
2020-01-31 00:28:08 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserInputDanielWidell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InstructionResponse;
+
+            Console.WriteLine("Hi, this is Daniel Widell.");
+        }
+    }
+}
